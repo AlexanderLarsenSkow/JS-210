bd2fdeac9600f6b77c2d2bf869185c483b5ed699
Alex <askow2015@gmail.com>
2025-05-21 11:56:03 -0600

    Finished prepations exercises

---
+++ book/prep/constructors.js
@@ -0,0 +1,9 @@
+// substring -- String, instance
+
+// create -- Object, can be used for many different types of constructors, static
+
+// fromCharCode() -- String, static
+
+// slice() -- Array, String, instance
+
+// toString() -- Object, many different types of constructors: Number, Array, String, Object, instance
---
+++ book/prep/style.js
@@ -0,0 +1,53 @@
+// Variable Names that Satify JS Naming Conventions
+
+// Perfect Score
+
+/*
+index - good, satisfies camelCase
+CatName - bad, should be catName
+snake_case - bad, only used for SCREAMING_CONSTANTS
+layzDog - good, satisfies camelCase
+quick_Fox - bad, mixes snake_case with camelCase
+1stCharacter - bad, causes syntax error, variables can't start with number
+operand2 - supposedly good, satisfies camelCase
+BIG_NUMBER - bad for variables, good for big constants.
+*/
+
+// Function Names
+
+/*
+index - good for basic function names
+CatName - good for Constructor functions
+snake_case - bad
+lazyDog - good for basic function names
+quick_Fox - bad
+1stCharacter - syntax error
+operand2 - good for basic function names
+BIG_NUMBER - bad
+*/
+
+// Magic Numbers
+
+/*
+index - bad
+CatName - bad
+snake_case - bad
+lazyDog - bad
+quick_Fox - bad
+1stCharacter - syntax error
+operand2 - bad
+BIG_NUMBER - good, only way for magic numbers.
+*/
+
+// Which can never be used?
+
+/*
+index - fine for variables and basic functions
+CatName - fine for constructor functions
+snake_case - not used.
+lazyDog - god for variables and basic functions.
+quick_Fox - not used.
+1stCharacter - syntax error, not used.
+operand2 - good for variables and basic functions.
+BIG_NUMBER - magic number constants.
+*/
